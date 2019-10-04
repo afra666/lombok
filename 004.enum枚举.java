4232fa5958f949f474228190cb3d76fe21591a30
afra666 <48674984+afra666@users.noreply.github.com>
2019-10-04 21:12:16 +0800

    Create 004.enum枚举.java

---
+++ 004.enum枚举.java
@@ -0,0 +1,19 @@
+package p2;
+
+enum Fruit {
+    orange,apple,banana
+}
+
+
+
+==========================================
+package p2;
+
+public class Caller {
+    public static void main(String[] args) {
+        Fruit myFruit=Fruit.orange;
+        System.out.println(myFruit);
+    }
+}
+==========================================
+orange
