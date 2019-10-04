be8b8ab35b5a1f735953925b5139c431b06df8e8
afra666 <48674984+afra666@users.noreply.github.com>
2019-10-04 20:21:22 +0800

    Update 001.setter and getter-normal.java

--- 001.setter and getter-normal.java
+++ 001.setter and getter-normal.java
@@ -1,20 +1,24 @@
-public class Person {
-private String name;
+public class Person {//类名
+private String name;//私有属性
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String name) {//setter方法用来设置属性值
+        this.name = name;//传入值
     }
-    public String getName(){
-        return this.name;
+    public String getName(){//getter传出值
+        return this.name;//返回属性值
     }
 }
+
 =======================================================
 
 public class Caller {
 
     public static void main(String[] args) {
         Person person=new Person();
-        person.setName("Handy");
-        System.out.println(person.getName());
+        person.setName("Handy");//设置属性值
+        System.out.println(person.getName());//查看getter属性值person.name
     }
 }
+
+====================输出
+Handy
