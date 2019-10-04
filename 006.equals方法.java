06bc8f03de37c1e498b72adc3fd389d023e1c5a6
afra666 <48674984+afra666@users.noreply.github.com>
2019-10-04 21:30:44 +0800

    Create 006.equals方法.java

---
+++ 006.equals方法.java
@@ -0,0 +1,12 @@
+package p3;
+
+public class Ha {
+    public static void main(String[] args) {
+
+       String a="Hello";
+       String b="Hello";
+        System.out.println(a.hashCode());
+        System.out.println(b.hashCode());
+        System.out.println(a.equals(b));
+    }
+}
